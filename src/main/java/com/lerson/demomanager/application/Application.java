52d9b8a045d701c8aa5a71be31c2434c85ea5a96
weblerson <zonejr4@gmail.com>
2023-06-26 13:38:57 -0400

    implementation of FXMLPath util class on Application

--- src/main/java/com/lerson/demomanager/application/Application.java
+++ src/main/java/com/lerson/demomanager/application/Application.java
@@ -1,5 +1,6 @@
 package com.lerson.demomanager.application;
 
+import com.lerson.demomanager.utils.FXMLPath;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -7,9 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader root = new FXMLLoader(getClass().getResource("main-view.fxml"));
+        FXMLLoader root = new FXMLLoader(getClass().getResource(FXMLPath.createFXMLPath("main-view.fxml")));
         Scene scene = new Scene(root.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
