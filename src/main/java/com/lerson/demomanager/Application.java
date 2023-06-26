33cafcf7f87bee8b0a5efa1245fc07115509515d
weblerson <zonejr4@gmail.com>
2023-06-26 12:47:26 -0400

    renamed HelloAplication.java to Application.java

--- src/main/java/com/lerson/demomanager/Application.java
+++ src/main/java/com/lerson/demomanager/Application.java
@@ -1,13 +1,12 @@
 package com.lerson.demomanager;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader root = new FXMLLoader(getClass().getResource("main-view.fxml"));
