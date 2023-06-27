b856a331bcb5e7ddbaddffd9339c1ad4823a2596
weblerson <zonejr4@gmail.com>
2023-06-27 15:18:55 -0400

    created util class for sha256 criptography

---
+++ src/main/java/com/lerson/demomanager/utils/SHA256.java
@@ -0,0 +1,33 @@
+package com.lerson.demomanager.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+
+    public static String parse(String text) {
+
+        StringBuilder hexString = null;
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+
+            hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('O');
+                }
+                hexString.append(hex);
+            }
+        }
+        catch (NoSuchAlgorithmException e) {
+            System.out.printf("\nError: %s", e.getMessage());
+        }
+
+        return hexString.toString();
+    }
+}
