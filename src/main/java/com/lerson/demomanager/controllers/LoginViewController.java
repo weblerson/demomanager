dcb7676625b27977f16365b684795e2e36233cf9
weblerson <zonejr4@gmail.com>
2023-06-27 15:32:54 -0400

    built LoginViewController struct

--- src/main/java/com/lerson/demomanager/controllers/LoginViewController.java
+++ src/main/java/com/lerson/demomanager/controllers/LoginViewController.java
@@ -1,4 +1,21 @@
 package com.lerson.demomanager.controllers;
 
+import com.lerson.demomanager.utils.SHA256;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginViewController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void submit() {
+        String username = usernameField.getText();
+        String hashPassword = SHA256.parse(passwordField.getText());
+    }
 }
