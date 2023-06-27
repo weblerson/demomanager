6748d0455c2511ba0ec0f5ec0d629e011f9234e1
weblerson <zonejr4@gmail.com>
2023-06-27 15:31:49 -0400

    created Employee and Job entities

---
+++ src/main/java/com/lerson/demomanager/entities/Employee.java
@@ -0,0 +1,89 @@
+package com.lerson.demomanager.entities;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private String cpf;
+    private LocalDate birthDate;
+    private String email;
+    private Boolean isAdmin;
+    private Double baseSalary;
+    private String username;
+
+    public Employee(Integer id, String name, String cpf, LocalDate birthDate,
+                    String email, Boolean isAdmin, Double baseSalary, String username) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.isAdmin = isAdmin;
+        this.baseSalary = baseSalary;
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee(" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", birthDate=" + birthDate +
+                ", email='" + email + '\'' +
+                ", isAdmin=" + isAdmin +
+                ", baseSalary=" + baseSalary +
+                ", username='" + username + '\'' +
+                ')';
+    }
+}
---
+++ src/main/java/com/lerson/demomanager/entities/Job.java
@@ -0,0 +1,71 @@
+package com.lerson.demomanager.entities;
+
+import java.time.LocalDateTime;
+
+public class Job {
+
+    private Integer id;
+    private String clientName;
+    private Integer type;
+    private Integer status;
+    private LocalDateTime deadline;
+    private String description;
+
+    public Job(Integer id, String clientName, Integer type,
+               Integer status, LocalDateTime deadline, String description) {
+        this.id = id;
+        this.clientName = clientName;
+        this.type = type;
+        this.status = status;
+        this.deadline = deadline;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Job(" +
+                "id=" + id +
+                ", clientName='" + clientName + '\'' +
+                ", type=" + type +
+                ", status=" + status +
+                ", deadline=" + deadline +
+                ", description='" + description + '\'' +
+                ')';
+    }
+}
