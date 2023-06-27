e0574322d345018a4467c056c5128d73f2ad2273
weblerson <zonejr4@gmail.com>
2023-06-27 15:17:46 -0400

    created personalized exceptions to db package

---
+++ src/main/java/com/lerson/demomanager/db/exceptions/DBException.java
@@ -0,0 +1,8 @@
+package com.lerson.demomanager.db.exceptions;
+
+public class DBException extends RuntimeException {
+
+    public DBException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/lerson/demomanager/db/exceptions/DBPropertiesNotFound.java
@@ -0,0 +1,8 @@
+package com.lerson.demomanager.db.exceptions;
+
+public class DBPropertiesNotFound extends RuntimeException {
+
+    public DBPropertiesNotFound(String msg) {
+        super(msg);
+    }
+}
