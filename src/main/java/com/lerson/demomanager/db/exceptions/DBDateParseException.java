92cbfc29e5f248e335ae6b389c2acfa81d52b0b2
weblerson <zonejr4@gmail.com>
2023-06-28 00:30:00 -0400

    created personalized RuntimeException "DBDateParseException"

---
+++ src/main/java/com/lerson/demomanager/db/exceptions/DBDateParseException.java
@@ -0,0 +1,8 @@
+package com.lerson.demomanager.db.exceptions;
+
+public class DBDateParseException extends RuntimeException {
+
+    public DBDateParseException(String msg) {
+        super(msg);
+    }
+}
