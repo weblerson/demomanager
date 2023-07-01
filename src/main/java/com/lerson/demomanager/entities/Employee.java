bd55b38f02bf86cce8a79ce39b4fd4512c2efda6
weblerson <zonejr4@gmail.com>
2023-07-01 18:37:02 -0400

    created new find overload to on Employee entity to implement findByUsernameAndPassword

--- src/main/java/com/lerson/demomanager/entities/Employee.java
+++ src/main/java/com/lerson/demomanager/entities/Employee.java
@@ -124,6 +124,12 @@ public class Employee {
         return employees;
     }
 
+    public DBQuery<Employee> find(String username, String password) {
+        DBQuery<Employee> employees = this.employeeDao.findByUsernameAndPassword(username, password);
+
+        return employees;
+    }
+
     public UpdateResult createEmployee() {
         UpdateResult updateResult = this.employeeDao.create(this);
 
